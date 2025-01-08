a3ef323908532fdbcd5f41c77726bd0a91a4b4d6
alirashidAR <alirashid.38@yahoo.com>
2025-01-09 00:22:58 +0530

    chore: replacing print with logging

--- src/open_data_pvnet/scripts/fetch_pvlive_data.py
+++ src/open_data_pvnet/scripts/fetch_pvlive_data.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from pvlive_api import PVLive
+import logging
 import pytz
+
+
+logger = logging.getLogger(__name__)
 
 class PVLiveData:
     def __init__(self):
@@ -14,7 +18,7 @@ class PVLiveData:
             df = self.pvl.latest(entity_type=entity_type, entity_id=entity_id, extra_fields=extra_fields, period=period, dataframe=True)
             return df
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(e)
             return None
 
     def get_data_between(self, start, end, entity_type="gsp", entity_id=0, extra_fields=""):
@@ -25,7 +29,7 @@ class PVLiveData:
             df = self.pvl.between(start=start, end=end, entity_type=entity_type, entity_id=entity_id, extra_fields=extra_fields, dataframe=True)
             return df
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(e)
             return None
 
     def get_data_at_time(self, dt):
@@ -36,24 +40,7 @@ class PVLiveData:
             df = self.pvl.at_time(dt, entity_type="pes", entity_id=0, extra_fields="", period=30, dataframe=True)
             return df
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(e)
             return None
 
 
-# Example usage
-pvl = PVLiveData()
-
-# Get the latest data
-latest_data = pvl.get_latest_data(period=30)
-print(latest_data)
-
-# Get the data between two dates
-start = datetime(2021, 1, 1, 12, 0, tzinfo=pytz.utc)
-end = datetime(2021, 1, 2, 12, 0, tzinfo=pytz.utc)
-data_between = pvl.get_data_between(start, end)
-print(data_between)
-
-# Get the data at a specific time
-dt = datetime(2021, 1, 1, 12, 0, tzinfo=pytz.utc)
-data_at_time = pvl.get_data_at_time(dt)
-print(data_at_time)
